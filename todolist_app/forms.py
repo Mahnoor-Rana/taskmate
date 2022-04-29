98089b9fc0a472f327143ee9fd4d47d130d79ccf
Mahnoor Rana <mahnoorrana2002@gmail.com>
2022-04-30 04:33:30 +0500

    uploading project

---
+++ todolist_app/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import field
+from django import forms
+from todolist_app.models import Tasklist
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Tasklist
+        fields= ['task','done']
